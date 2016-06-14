ff99e658214e200c09d6842a8310670cdead20c5
anushamc <anushamc@yahoo.com>
2016-06-14 06:38:13 +0100

    create template skeleton

--- code/county_stats/accidents/views.py
+++ code/county_stats/accidents/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'accidents/index.html'
+    
+    def get_context_data(self, **kwargs):
+        return kwargs
