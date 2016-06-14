da0376e196951002d8422424314b60632e2ef23c
anushamc <anushamc@yahoo.com>
2016-06-14 13:15:08 +0100

    bugfix: step 8

--- code/county_stats/accidents/static/accidents/js/accidents.js
+++ code/county_stats/accidents/static/accidents/js/accidents.js
@@ -3,15 +3,24 @@
         source: new ol.source.OSM()
       });
 
-      var vector = new ol.layer.Vector({
+      var county = new ol.layer.Vector({
         source: new ol.source.Vector({
-          url: '/accidents/accidentsGeojson/',
+          url: '/accidents/countyGeoJSON/',
           format: new ol.format.GeoJSON()
         })
       });
 
+      var accident_source = new ol.source.Vector({
+          url: '/accidents/accidentsGeojson/?county=null',
+          format: new ol.format.GeoJSON()
+        });
+
+      var accident = new ol.layer.Vector({
+        source: accident_source
+      });
+
       var map = new ol.Map({
-        layers: [raster, vector],
+        layers: [raster, county],
         target: 'map',
         view: new ol.View({
           center: [0, 0],
@@ -19,12 +28,14 @@
         })
       });
 
-      var select = new ol.interaction.Select();
+      var select = new ol.interaction.Select({
+        layers:[county]
+      });
 
       map.addInteraction(select);
       select.on('select', function(e) {
-        document.getElementById('comments').innerHTML = '&nbsp;' +
-            e.target.getFeatures().getLength() +
-            ' selected features (last operation selected ' + e.selected.length +
-            ' and deselected ' + e.deselected.length + ' features)';
+        accident.setSource({
+          url: '/accidents/accidentsGeojson/?county='+e.target.getFeatures().getArray()[0].get('name').replace(' ','%20'),
+          format: new ol.format.GeoJSON()
+        });
       });
--- code/county_stats/accidents/views.py
+++ code/county_stats/accidents/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import View, TemplateView
 from django.core.serializers import serialize
-from .models import County
+from .models import County, Accident
 from django.http.response import HttpResponse
 
 class IndexView(TemplateView):
@@ -16,4 +16,22 @@ class countyGeoJSON(View):
     	geojson = serialize('geojson', County.objects.all(),
           geometry_field='geom',
           fields=('name',))
+    	return HttpResponse(geojson)
+
+
+class AccidentsGeojson(View):
+    def get(self, request):
+    	#get county from county name in request
+    	county = County.objects.filter(name=request.GET.get('county')).first()
+    	#if no geom fields exist, create them for accident
+    	#migrations would be better, but are more complex
+    	no_accident_exists = Accident.objects.filter(geom__isnull=False).exists()
+    	if no_accident_exists:
+    		Accident.objects.raw('UPDATE accidents_accident SET geom=ST_SIMPLIFY(ST_GeomFromText(\'POINT(\' || %s || \' \' || %s || \')\',27700);',[location_easting_osgr, location_northing_osgr])
+    	#use within query to filter all points within the geometry
+    	geojson = {}
+    	if county:
+	    	geojson = serialize('geojson', Accident.objects.filter(geom__within=county.geom),
+	          geometry_field='geom',
+	          fields=('accident_index', 'accident_severity'))
     	return HttpResponse(geojson)
