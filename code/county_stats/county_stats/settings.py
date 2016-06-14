73802609b223fb6dc737c48059d12ececf2eab7c
anushamc <anushamc@yahoo.com>
2016-06-14 04:43:26 +0100

    update settings to include library files

--- code/county_stats/county_stats/settings.py
+++ code/county_stats/county_stats/settings.py
@@ -88,3 +88,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'accidents','static'),
+)
